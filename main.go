130ef497b0a9c4a0d2a8179ec5e24a1d97a2ee11
hixi4 <167585616+hixi4@users.noreply.github.com>
2024-06-16 20:58:50 +0300

    Update main.go

--- main.go
+++ main.go
@@ -1,1 +1,21 @@
+package main
 
+import (
+	"case-transport/transport"
+
+	"case-transport/route"
+)
+
+func main() {
+	bus := transport.Bus{}
+	train := transport.Train{}
+	plane := transport.Plane{}
+
+	travelRoute := route.Route{}
+	travelRoute.AddVehicle(bus)
+	travelRoute.AddVehicle(train)
+	travelRoute.AddVehicle(plane)
+
+	travelRoute.ShowVehicles()
+	travelRoute.Travel()
+}
