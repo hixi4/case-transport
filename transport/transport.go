8bfbd981a9223d2d3c63b9dd60d3fff0aa30dc04
hixi4 <167585616+hixi4@users.noreply.github.com>
2024-06-16 21:01:41 +0300

    Update transport.go

--- transport/transport.go
+++ transport/transport.go
@@ -1,1 +1,42 @@
+package transport
 
+import "fmt"
+
+// PublicTransport інтерфейс для транспортних засобів
+type PublicTransport interface {
+	BoardPassengers()
+	UnboardPassengers()
+}
+
+// Bus структура для автобуса
+type Bus struct{}
+
+func (b Bus) BoardPassengers() {
+	fmt.Println("Автобус приймає пасажирів.")
+}
+
+func (b Bus) UnboardPassengers() {
+	fmt.Println("Автобус висаджує пасажирів.")
+}
+
+// Train структура для поїзду
+type Train struct{}
+
+func (t Train) BoardPassengers() {
+	fmt.Println("Потяг приймає пасажирів.")
+}
+
+func (t Train) UnboardPassengers() {
+	fmt.Println("Потяг висаджує пасажирів.")
+}
+
+// Plane структура для літака
+type Plane struct{}
+
+func (p Plane) BoardPassengers() {
+	fmt.Println("Літак приймає пасажирів.")
+}
+
+func (p Plane) UnboardPassengers() {
+	fmt.Println("Літак высаджує пасажирів.")
+}
